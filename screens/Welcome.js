45343234a836567527f21e2ca813b40829c1dafe
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-03-07 11:56:03 +0100

    button added

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,Image } from 'react-native'
+import { StyleSheet, Text, View,Image,TouchableOpacity } from 'react-native'
 import React from 'react'
 import Lottie from 'lottie-react-native';
 
@@ -12,6 +12,10 @@ const Welcome = () => {
 
       <Text style={styles.mainText}>Check the weather in your Location</Text>
       <Text style={styles.subText}>Selcct your current location and get the weather forcast in seconds</Text>
+        <TouchableOpacity onPress={''} style={styles.ButtonContainer}>
+            <Text style={styles.ButtonText}>Get Started</Text>
+        </TouchableOpacity>
+    
     </View>
   )
 }
@@ -42,11 +46,28 @@ const styles = StyleSheet.create({
         textAlign:'center', 
         paddingHorizontal:50,
         color:'#818589',
-        lineHeight:20
+        lineHeight:20,
+        paddingBottom:30,
+    },
+
+    ButtonContainer: {
+        backgroundColor: "#00569D",
+        borderRadius: 5,
+        paddingVertical: 20,
+        paddingHorizontal: 140
+    },
+
+
+    ButtonText: {
+        fontSize: 15,
+        color: "#fff",
+        fontWeight: 500,
+        alignSelf: "center",
+        // textTransform: "uppercase"
     },
 
     weatherAnimation:{
-        width:200,
+        width:350,
         resizeMode:'contain',
     }
   });
